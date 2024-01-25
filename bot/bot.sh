4088729b5457f4df736730130640d867b2085cde
NIXY Project <152050497+nixyproject-cfd@users.noreply.github.com>
2024-01-26 01:13:07 +0700

    .

--- bot/bot.sh
+++ bot/bot.sh
@@ -9,13 +9,13 @@ NC='\e[0m'
 apt update && apt upgrade
 apt install python3 python3-pip git
 cd /usr/bin
-wget https://raw.githubusercontent.com/ndracenter/scprem/main/bot/bot.zip
+wget https://raw.githubusercontent.com/nixyproject-cfd/asp/main/bot/bot.zip
 unzip bot.zip
 mv bot/* /usr/bin
 chmod +x /usr/bin/*
 rm -rf bot.zip
 clear
-wget https://raw.githubusercontent.com/ndracenter/scprem/main/bot/kyt.zip
+wget https://raw.githubusercontent.com/nixyproject-cfd/asp/main/bot/kyt.zip
 unzip kyt.zip
 pip3 install -r kyt/requirements.txt
 
